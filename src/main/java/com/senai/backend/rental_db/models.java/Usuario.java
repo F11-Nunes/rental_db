18244571d4644661f4273de0583f8677eeb7f175
F11-Nunes <lipinho.nunes2019@gmail.com>
2026-08-10 11:18:10 -0300

    changes 1

---
+++ src/main/java/com/senai/backend/rental_db/models.java/Usuario.java
@@ -0,0 +1,5 @@
+package com.senai.backend.rental_db.models.java;
+
+public class Usuario {
+    
+}
---
+++ src/test/java/com/senai/backend/rental_db/RentalDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.backend.rental_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
